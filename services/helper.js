5d8a5509d364be583678520884814b4bf46f70f5
Paulo Eduardo Marchini Nery <nery@titi.net.br>
2017-02-13 20:08:56 -0200

    Inclusão de Terapeuta Ocupacional

--- services/helper.js
+++ services/helper.js
@@ -205,6 +205,10 @@ function helperService(baseService) {
       id: '4',
       name: 'Fisioterapeuta Cuidador'
     },
+    {
+      id: '5',
+      name: 'Terapeuta Ocupacional'
+    },
 		{
 			id: '6',
 			name: 'Educador Físico'
@@ -213,11 +217,6 @@ function helperService(baseService) {
       id: '0',
       name: 'Todos'
     }
-		/*,
-    {
-      id: '5',
-      name: 'Fonoaudiólogo'
-    }*/
   ];
 
   helper.periodoOptions = [
